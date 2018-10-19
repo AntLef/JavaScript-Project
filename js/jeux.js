0c7414a4ff8eb631f041d6a18aef70fa2eda0569
Lowik95 <44092318+Lowik95@users.noreply.github.com>
2018-10-19 04:29:11 +0200

    Add files via upload

--- js/jeux.js
+++ js/jeux.js
@@ -19,16 +19,40 @@ var rcsr = document.getElementById('CASERNE');
 var recurie = document.getElementById('re');
 var reci = document.getElementById('ECURIE');
 
-var recurie = document.getElementById('ra');
+var ratelier = document.getElementById('ra');
 var ratl = document.getElementById('ATELIER');
 
-var recurie = document.getElementById('rf');
+var rforge = document.getElementById('rf');
 var rfog = document.getElementById('FORGE');
 
-var BOIS = 0;
-var ARGILE = 0;
-var FER = 0;
-var CÉREALE = 0;
+var rtroupedef = document.getElementById('rtd');
+var rtdef = document.getElementById('DEF');
+
+var rtroupeoff = document.getElementById('rtof');
+var rtoff = document.getElementById('OFF');
+
+var rtroupetireur = document.getElementById('rtir');
+var rttireur = document.getElementById('TIREUR');
+
+var rcavaleriespy = document.getElementById('rspy');
+var rtspy = document.getElementById('SPY');
+
+var rcavaleriedef = document.getElementById('rdef');
+var rcdef = document.getElementById('CDEF');
+
+//var BOIS = 750;
+//var ARGILE = 750;
+//var FER = 750;
+//var CÉREALE = 750;
+
+var ressources = {
+    argile: 750,
+    fer:750,
+    bois:750,
+    céréale:750,
+};
+
+
 
 var timerBois = 0;
 var timerArgile = 0;
@@ -48,9 +72,12 @@ var SILO_VALEUR = 800;
 
 var CASERNE_PRINCIPAL = 0;
 var CASERNE_LEVEL = 0;
+var CASERNE_UNLOCK_OFF = 3;
+var CASERNE_UNLOCK_TIREUR = 5;
 
 var ECURIE_PRINCIPAL = 0;
 var ECURIE_LEVEL = 0;
+var ECURIE_UNLOCK_DEF = 5;
 
 var ATELIER_PRINCIPAL = 0;
 var ATELIER_LEVEL = 0;
@@ -65,67 +92,76 @@ var multiplyForge = 1;
 var multiply = 1;
 var multiplyDepot = 2;
 var multiplySilo = 2;
-
 var Population = 2;
+
+//Variable pour les troupes
+
+var TROUPE_DEF = 0;
+var TROUPE_OFF = 0;
+var TROUPE_NB_OFF = 0;
+var TROUPE_TIREUR = 0;
+var TROUPE_NB_TIREUR = 0;
+var CAVALERIE_SPY = 0;
+var CAVALERIE_NB_DEF =0;
 
 
 function bois() {
-    if (timerBois && (BOIS >= DEPOT_VALEUR)) {
+    if (timerBois && (ressources.bois >= DEPOT_VALEUR)) {
         clearTimeout(timerBois);
         timerBois = setTimeout(function(){
-        rbois.innerHTML = BOIS;
+        rbois.innerHTML = ressources.bois;
     bois();
 },0);
     }
 timerBois = setTimeout(function(){
-    BOIS++;
-        rbois.innerHTML = BOIS;
+    ressources.bois++;
+        rbois.innerHTML = ressources.bois;
     bois();
 },1);
 }
 
 
 function argile() {
-    if (timerArgile && (ARGILE >= DEPOT_VALEUR)) {
+    if (timerArgile && (ressources.argile >= DEPOT_VALEUR)) {
         clearTimeout(timerArgile);
         timerArgile = setTimeout(function(){
-        rargile.innerHTML = ARGILE;
+        rargile.innerHTML = ressources.argile;
     argile();
 },0);
     }
 timerArgile = setTimeout(function(){
-    ARGILE++;
-        rargile.innerHTML = ARGILE;
+    ressources.argile++;
+        rargile.innerHTML = ressources.argile;
     argile();
 },1);
 }
 
 function fer() {
-    if (timerFer && (FER >= DEPOT_VALEUR)) {
+    if (timerFer && (ressources.fer >= DEPOT_VALEUR)) {
         clearTimeout(timerFer);
         timerFer = setTimeout(function(){
-        rfer.innerHTML = FER;
+        rfer.innerHTML = ressources.fer;
     fer();
-},1);
+},0);
     }
 timerFer = setTimeout(function(){
-    FER++;
-        rfer.innerHTML = FER;
+    ressources.fer++;
+        rfer.innerHTML = ressources.fer;
     fer();
 },1);
 }
 
 function céreale() {
-    if (timerCéreale && (CÉREALE >= SILO_VALEUR)) {
+    if (timerCéreale && (ressources.céréale >= SILO_VALEUR)) {
         clearTimeout(timerCéreale);
         timerCéreale = setTimeout(function(){
-        rcéreale.innerHTML = CÉREALE;
+        rcéreale.innerHTML = ressources.céréale;
     céreale();
 },0);
     }
 timerCéreale = setTimeout(function(){
-    CÉREALE++;
-        rcéreale.innerHTML = CÉREALE;
+    ressources.céréale++;
+        rcéreale.innerHTML = ressources.céréale;
     céreale();
 },1);
 }
@@ -133,15 +169,15 @@ timerCéreale = setTimeout(function(){
 
 function BatimentPrincipal(){
 
-    if(BOIS >= (200 * multiply) && ARGILE >= (150 * multiply) && FER >= (110 * multiply) && CÉREALE >= (90 * multiply) && (BATIMENT_PRINCIPAL == BATIMENT_LEVEL)){ 
+    if(ressources.bois >= (200 * multiply) && ressources.argile >= (150 * multiply) && ressources.fer >= (110 * multiply) && ressources.céréale >= (90 * multiply) && (BATIMENT_PRINCIPAL == BATIMENT_LEVEL)){ 
         //console.log("ca marche")
         BATIMENT_PRINCIPAL++;
         BATIMENT_LEVEL++;
 
-        BOIS = BOIS - (200 * multiply);
-        ARGILE = ARGILE-(150 * multiply);
-        FER = FER-(110 * multiply);
-        CÉREALE = CÉREALE-(90 * multiply);
+        ressources.bois = ressources.bois - (200 * multiply);
+        ressources.argile = ressources.argile-(150 * multiply);
+        ressources.fer = ressources.fer-(110 * multiply);
+        ressources.céréale = ressources.céréale-(90 * multiply);
         //console.log(BATIMENT_PRINCIPAL);
         rbp.innerHTML = BATIMENT_PRINCIPAL;
         multiply++
@@ -153,14 +189,14 @@ function BatimentPrincipal(){
 
 
 function DepotPrincipal(){
-        if(BOIS >= (140 * multiplyDepot) && ARGILE >= (160 * multiplyDepot) && FER >= (120 * multiplyDepot) && CÉREALE >= (100 * multiplyDepot) && (DEPOT_PRINCIPAL == DEPOT_LEVEL)){ 
+        if(ressources.bois >= (140 * multiplyDepot) && ressources.argile >= (160 * multiplyDepot) && ressources.fer >= (120 * multiplyDepot) && ressources.céréale >= (100 * multiplyDepot) && (DEPOT_PRINCIPAL == DEPOT_LEVEL)){ 
             DEPOT_PRINCIPAL++;
             DEPOT_LEVEL++;
 
-            BOIS = BOIS - (140 * multiplyDepot);
-            ARGILE = ARGILE-(160 * multiplyDepot);
-            FER = FER-(120 * multiplyDepot);
-            CÉREALE = CÉREALE-(100 * multiplyDepot);
+            ressources.bois = ressources.bois - (140 * multiplyDepot);
+            ressources.argile = ressources.argile-(160 * multiplyDepot);
+            ressources.fer = ressources.fer-(120 * multiplyDepot);
+            ressources.céréale = ressources.céréale-(100 * multiplyDepot);
 
             rdp.innerHTML = DEPOT_PRINCIPAL;
             multiplyDepot++;
@@ -176,14 +212,14 @@ function DepotPrincipal(){
 
 
 function SiloPrincipal(){
-        if(BOIS >= (100 * multiplySilo) && ARGILE >= (170 * multiplySilo) && FER >= (130 * multiplySilo) && CÉREALE >= (190 * multiplySilo) && (SILO_PRINCIPAL == SILO_LEVEL)){ 
+        if(ressources.bois >= (100 * multiplySilo) && ressources.argile >= (170 * multiplySilo) && ressources.fer >= (130 * multiplySilo) && ressources.céréale >= (190 * multiplySilo) && (SILO_PRINCIPAL == SILO_LEVEL)){ 
             SILO_PRINCIPAL++;
             SILO_LEVEL++;
 
-            BOIS = BOIS - (100 * multiplySilo);
-            ARGILE = ARGILE-(170 * multiplySilo);
-            FER = FER-(130 * multiplySilo);
-            CÉREALE = CÉREALE-(190 * multiplySilo);
+            ressources.bois = ressources.bois - (100 * multiplySilo);
+            ressources.argile = ressources.argile-(170 * multiplySilo);
+            ressources.fer = ressources.fer-(130 * multiplySilo);
+            ressources.céréale = ressources.céréale-(190 * multiplySilo);
 
             rsl.innerHTML = SILO_PRINCIPAL;
             multiplySilo++;
@@ -199,14 +235,16 @@ function SiloPrincipal(){
 
 
 function CaserneUp(){
-        if(BOIS >= (80 * multiplyCaserne) && ARGILE >= (60 * multiplyCaserne) && FER >= (170 * multiplyCaserne) && CÉREALE >= (100 * multiplyCaserne) && (CASERNE_PRINCIPAL == CASERNE_LEVEL)){ 
+        if(ressources.bois >= (80 * multiplyCaserne) && ressources.argile >= (60 * multiplyCaserne) && ressources.fer >= (170 * multiplyCaserne) && ressources.céréale >= (100 * multiplyCaserne) && (CASERNE_PRINCIPAL == CASERNE_LEVEL)){ 
             CASERNE_PRINCIPAL++;
             CASERNE_LEVEL++;
+            TROUPE_NB_OFF++;
+            TROUPE_NB_TIREUR++;
 
-            BOIS = BOIS - (80 * multiplyCaserne);
-            ARGILE = ARGILE-(60 * multiplyCaserne);
-            FER = FER-(170 * multiplyCaserne);
-            CÉREALE = CÉREALE-(100 * multiplyCaserne);
+            ressources.bois = ressources.bois - (80 * multiplyCaserne);
+            ressources.argile = ressources.argile-(60 * multiplyCaserne);
+            ressources.fer = ressources.fer-(170 * multiplyCaserne);
+            ressources.céréale = ressources.céréale-(100 * multiplyCaserne);
 
             rcsr.innerHTML = CASERNE_PRINCIPAL;
             multiplyCaserne++;
@@ -217,14 +255,15 @@ function CaserneUp(){
     }
 
 function EcurieUp(){
-        if(BOIS >= (200 * multiplyEcurie) && ARGILE >= (100 * multiplyEcurie) && FER >= (150 * multiplyEcurie) && CÉREALE >= (270 * multiplyEcurie) && (ECURIE_PRINCIPAL == ECURIE_LEVEL)){ 
+        if(ressources.bois >= (200 * multiplyEcurie) && ressources.argile >= (100 * multiplyEcurie) && ressources.fer >= (150 * multiplyEcurie) && ressources.céréale >= (270 * multiplyEcurie) && (ECURIE_PRINCIPAL == ECURIE_LEVEL)){ 
             ECURIE_PRINCIPAL++;
             ECURIE_LEVEL++;
+            CAVALERIE_NB_DEF++;
 
-            BOIS = BOIS - (200 * multiplyEcurie);
-            ARGILE = ARGILE-(100 * multiplyEcurie);
-            FER = FER-(150 * multiplyEcurie);
-            CÉREALE = CÉREALE-(270 * multiplyEcurie);
+            ressources.bois = ressources.bois - (200 * multiplyEcurie);
+            ressources.argile = ressources.argile-(100 * multiplyEcurie);
+            ressources.fer = ressources.fer-(150 * multiplyEcurie);
+            ressources.céréale = ressources.céréale-(270 * multiplyEcurie);
 
             reci.innerHTML = ECURIE_PRINCIPAL;
             multiplyEcurie++;
@@ -235,14 +274,14 @@ function EcurieUp(){
     }
 
 function AtelierUp(){
-        if(BOIS >= (300 * multiplyAtelier) && ARGILE >= (100 * multiplyAtelier) && FER >= (470 * multiplyAtelier) && CÉREALE >= (210 * multiplyAtelier) && (ATELIER_PRINCIPAL == ATELIER_LEVEL)){ 
+        if(ressources.bois >= (300 * multiplyAtelier) && ressources.argile >= (100 * multiplyAtelier) && ressources.fer >= (470 * multiplyAtelier) && ressources.céréale >= (210 * multiplyAtelier) && (ATELIER_PRINCIPAL == ATELIER_LEVEL)){ 
             ATELIER_PRINCIPAL++;
             ATELIER_LEVEL++;
 
-            BOIS = BOIS - (300 * multiplyAtelier);
-            ARGILE = ARGILE-(100 * multiplyAtelier);
-            FER = FER-(470 * multiplyAtelier);
-            CÉREALE = CÉREALE-(210 * multiplyAtelier);
+            ressources.bois = ressources.bois - (300 * multiplyAtelier);
+            ressources.argile = ressources.argile-(100 * multiplyAtelier);
+            ressources.ressources.fer = FER-(470 * multiplyAtelier);
+            ressources.céréale = ressources.céréale-(210 * multiplyAtelier);
 
             ratl.innerHTML = ATELIER_PRINCIPAL;
             multiplyAtelier++;
@@ -253,14 +292,14 @@ function AtelierUp(){
     }
 
 function ForgeUp(){
-        if(BOIS >= (100 * multiplyForge) && ARGILE >= (100 * multiplyForge) && FER >= (400 * multiplyForge) && CÉREALE >= (100 * multiplyForge) && (FORGE_PRINCIPAL == FORGE_LEVEL)){ 
+        if(ressources.bois >= (100 * multiplyForge) && ressources.argile >= (100 * multiplyForge) && ressources.fer >= (400 * multiplyForge) && ressources.céréale >= (100 * multiplyForge) && (FORGE_PRINCIPAL == FORGE_LEVEL)){ 
             FORGE_PRINCIPAL++;
             FORGE_LEVEL++;
 
-            BOIS = BOIS - (100 * multiplyForge);
-            ARGILE = ARGILE-(100 * multiplyForge);
-            FER = FER-(400 * multiplyForge);
-            CÉREALE = CÉREALE-(100 * multiplyForge);
+            ressources.bois = ressources.bois - (100 * multiplyForge);
+            ressources.argile = ressources.argile-(100 * multiplyForge);
+            ressources.fer = ressources.fer-(400 * multiplyForge);
+            ressources.céréale = ressources.céréale-(100 * multiplyForge);
 
             rfog.innerHTML = FORGE_PRINCIPAL;
             multiplyForge++;
@@ -270,6 +309,123 @@ function ForgeUp(){
         }
     }
 
+
+function TroupeDef(){
+        if(ressources.bois >= 100 && ressources.argile >= 100 && ressources.fer >= 100 && ressources.céréale >= 100 && (TROUPE_DEF <= CASERNE_LEVEL)){ 
+            TROUPE_DEF++;
+
+            ressources.bois = ressources.bois - 100 ;
+            ressources.argile = ressources.argile-100 ;
+            ressources.fer = ressources.fer-100 ;
+            ressources.céréale = ressources.céréale-100 ;
+
+            rtdef.innerHTML = TROUPE_DEF;
+        }
+    }
+
+function TroupeOff(){
+    if(TROUPE_NB_OFF >= CASERNE_UNLOCK_OFF){ 
+        if(ressources.bois >= 100 && ressources.argile >= 100 && ressources.fer >= 100 && ressources.céréale >= 100){ 
+            TROUPE_OFF++;
+
+            ressources.bois = ressources.bois - 100 ;
+            ressources.argile = ressources.argile-100 ;
+            ressources.fer = ressources.fer-100 ;
+            ressources.céréale = ressources.céréale-100 ;
+
+            rtoff.innerHTML = TROUPE_OFF;
+        }
+    }
+        else{
+            alert("Il faut que ta caserne soit niveau 3 pour débloquer les trouoes off");
+        }
+    }
+
+function TroupeTireur(){
+    if(TROUPE_NB_TIREUR >= CASERNE_UNLOCK_TIREUR){ 
+        if(ressources.bois >= 200 && ressources.argile >= 100 && ressources.fer >= 250 && ressources.céréale >= 150 ){ 
+            TROUPE_TIREUR++;
+
+            ressources.bois = ressources.bois - 200 ;
+            ressources.argile = ARGILE - 100 ;
+            ressources.fer = ressources.fer - 250 ;
+            ressources.céréale = ressources.céréale - 150 ;
+
+            rttireur.innerHTML = TROUPE_TIREUR;
+        }
+    }
+    else{
+        alert("Il faut que ta caserne soit niveau 5 pour débloquer les trouoes tireur");
+        }
+    }
+
+function CavalerieSpy(){
+        if(ressources.bois >= 100 && ressources.argile >= 100 && ressources.fer >= 100 && ressources.céréale >= 100 && (CAVALERIE_SPY >= ECURIE_LEVEL)){ 
+            CAVALERIE_SPY++;
+
+            ressources.bois = ressources.bois - 100 ;
+            ressources.argile = ressources.argile-100 ;
+            ressources.fer = ressources.fer-100 ;
+            ressources.céréale = ressources.céréale-100 ;
+
+            rtspy.innerHTML = CAVALERIE_SPY;
+        }
+    }
+
+function CavalerieDef(){
+    if(CAVALERIE_NB_DEF >= ECURIE_UNLOCK_DEF){ 
+        if(ressources.bois >= 200 && ressources.argile >= 100 && ressources.fer >= 250 && ressources.céréale >= 150 ){ 
+            CAVALERIE_DEF++;
+
+            ressources.bois = ressources.bois - 200 ;
+            ressources.argile = ressources.argile - 100 ;
+            ressources.fer = ressources.fer - 250 ;
+            ressources.céréale = ressources.céréale - 150 ;
+
+            rcdef.innerHTML = CAVALERIE_DEF;
+        }
+    }
+    else{
+        alert("Il faut que ton Ecurie soit niveau 5 pour débloquer la cavalerie Def");
+        }
+    }
+/*
+function TroupeTireur(){
+    if(TROUPE_NB_TIREUR >= CASERNE_UNLOCK_TIREUR){ 
+        if(BOIS >= 200 && ARGILE >= 100 && FER >= 250 && CÉREALE >= 150 ){ 
+            TROUPE_TIREUR++;
+
+            BOIS = BOIS - 200 ;
+            ARGILE = ARGILE - 100 ;
+            FER = FER - 250 ;
+            CÉREALE = CÉREALE - 150 ;
+
+            rttireur.innerHTML = TROUPE_TIREUR;
+        }
+    }
+    else{
+        alert("Il faut que ta caserne soit niveau 5 pour débloquer les trouoes tireur");
+        }
+    }
+
+function TroupeTireur(){
+    if(TROUPE_NB_TIREUR >= CASERNE_UNLOCK_TIREUR){ 
+        if(BOIS >= 200 && ARGILE >= 100 && FER >= 250 && CÉREALE >= 150 ){ 
+            TROUPE_TIREUR++;
+
+            BOIS = BOIS - 200 ;
+            ARGILE = ARGILE - 100 ;
+            FER = FER - 250 ;
+            CÉREALE = CÉREALE - 150 ;
+
+            rttireur.innerHTML = TROUPE_TIREUR;
+        }
+    }
+    else{
+        alert("Il faut que ta caserne soit niveau 5 pour débloquer les trouoes tireur");
+        }
+    }
+*/
 bois();
 argile();
 fer();
